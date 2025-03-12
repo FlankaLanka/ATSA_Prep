fa05908a560b4ab653177d1e42dfe45491004d66
Frank Yang <frankyang233@gmail.com>
2025-03-12 15:36:14 -0400

    variables working

--- Assets/Scripts/VariablesManager.cs
+++ Assets/Scripts/VariablesManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class VariablesManager : MonoBehaviour
 {
@@ -41,6 +43,7 @@ public class VariablesManager : MonoBehaviour
     [Header("Game Logic Related")]
     public int score, total;
     public int[] abc = new int[3];
+    public string[] abcExp = new string[3];
 
     public void StartVariables()
     {
@@ -50,17 +53,21 @@ public class VariablesManager : MonoBehaviour
         timePerVariable = TranslateDropdownTimeVariable(timePerVariableDropdown.value);
         score = 0;
         total = 0;
-
+        EnableAppropriateUI();
         gameRunning = true;
-
-        //gameLoopCoroutine = StartCoroutine(RunGameCoroutine());
     }
 
     public void StopVariables()
     {
         StopCoroutine(gameLoopCoroutine);
-
         gameRunning = false;
+
+        timerText.gameObject.SetActive(true);
+        timerText.text = "--:---";
+        expression.text = "-";
+        correctnessIndicator.gameObject.SetActive(true);
+        correctnessIndicator.color = Color.white;
+
         settingsMenu.SetActive(true);
     }
 
@@ -96,7 +103,7 @@ public class VariablesManager : MonoBehaviour
         correctnessIndicator.color = Color.white;
         for(int i = 0; i < 3; i++)
         {
-            expression.text = (char)('A' + i) + " = " + abc[i];
+            expression.text = (char)('A' + i) + " = " + abcExp[i];
             timer = 0;
             while (timer < timePerVariable)
             {
@@ -105,15 +112,42 @@ public class VariablesManager : MonoBehaviour
                 yield return null;
             }
         }
+        total += 3;
 
         //waiting for answer
-        for(int i = 0; i < 3; i++)
+        List<int> randOrder = new List<int> { 0, 1, 2 }; //find a random order to ask for ABC
+        Shuffle(randOrder);
+        for (int i = 0; i < 3; i++)
         {
+            expression.text = (char)('A' + randOrder[i]) + " = ?";
+            timer = 0f;
+            while (timer < timePerVariable)
+            {
+                KeyCode? key = GetCurrentKeypadPressed();
+                if (key != null)
+                {
+                    if (key - KeyCode.Keypad0 == abc[randOrder[i]])
+                    {
+                        score++;
+                        correctnessIndicator.color = Color.green;
+                    }
+                    else
+                    {
+                        correctnessIndicator.color = Color.red;
+                    }
 
+                    timer = timePerVariable; //if recieved an answer, use timer to immediately go to next
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                }
+                timerText.text = GlobalFormatter.FormatTimeSecMilli(timePerVariable - timer);
+                yield return null;
+            }
         }
 
-
-
+        //helps move on to next question in Update
         gameLoopCoroutine = null;
     }
 
@@ -124,12 +158,74 @@ public class VariablesManager : MonoBehaviour
 
     private void CalculateABC()
     {
-        abc[0] = Random.Range(1, 5);
-        abc[1] = Random.Range(1, 5);
-        abc[2] = Random.Range(1, 5);
+        int qType = Random.Range(0, 3);
+        if(qType == 0) //no algebra
+        {
+            abc[0] = Random.Range(1, 5);
+            abc[1] = Random.Range(1, 5);
+            abc[2] = Random.Range(1, 5);
+            abcExp[0] = abc[0].ToString();
+            abcExp[1] = abc[1].ToString();
+            abcExp[2] = abc[2].ToString();
+        }
+        else if (qType == 1) //1 algebra
+        {
+            List<int> r = new List<int> { 0, 1, 2 }; //find a random order to ask for ABC
+            Shuffle(r);
+
+            abc[r[0]] = Random.Range(1, 5);
+            abc[r[1]] = Random.Range(1, 5);
+            abcExp[r[0]] = abc[r[0]].ToString();
+            abcExp[r[1]] = abc[r[1]].ToString();
+
+            GenerateWorkingEquation(r, 0, 2);
+
+            ////loop until we find a possible fit for the equation
+            //int op;
+            //int val;
+            //(bool, int) result;
+            //do
+            //{
+            //    op = Random.Range(0, 4);
+            //    val = Random.Range(1, 3);
+            //    result = IsOperationValid(abc[r[0]], op, val);
+            //} while (!result.Item1) ;
+
+            //abc[r[2]] = result.Item2;
+            //abcExp[r[2]] = ((char)(65 + r[0])).ToString() + GetOperationCharacter(op).ToString() + val.ToString();
+        }
+        else //2 algebra
+        {
+            List<int> r = new List<int> { 0, 1, 2 }; //find a random order to ask for ABC
+            Shuffle(r);
+
+            abc[r[0]] = Random.Range(1, 5);
+            abcExp[r[0]] = abc[r[0]].ToString();
+
+            GenerateWorkingEquation(r, 0, 1);
+            GenerateWorkingEquation(r, 1, 2);
+        }
+
+
     }
 
     #region Helpers
+
+    private void GenerateWorkingEquation(List<int> r, int idxToCheck, int idxToStore)
+    {
+        int op;
+        int val;
+        (bool, int) result;
+        do
+        {
+            op = Random.Range(0, 4);
+            val = Random.Range(1, 3);
+            result = IsOperationValid(abc[r[idxToCheck]], op, val);
+        } while (!result.Item1);
+
+        abc[r[idxToStore]] = result.Item2;
+        abcExp[r[idxToStore]] = ((char)(65 + r[idxToCheck])).ToString() + GetOperationCharacter(op).ToString() + val.ToString();
+    }
 
 
     private float TranslateDropdownTimeVariable(int val)
@@ -166,6 +262,62 @@ public class VariablesManager : MonoBehaviour
             correctnessIndicator.gameObject.SetActive(true);
         else
             correctnessIndicator.gameObject.SetActive(false);
+    }
+
+    void Shuffle<T>(List<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
+
+    private KeyCode? GetCurrentKeypadPressed()
+    {
+        for (KeyCode key = KeyCode.Keypad0; key <= KeyCode.Keypad9; key++)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                return key;
+            }
+        }
+
+        return null;
+    }
+
+    private (bool,int) IsOperationValid(int start, int op, int val)
+    {
+        switch(op)
+        {
+            case 0: //+
+                return (start + val <= 9, start + val);
+            case 1: //-
+                return (start - val >= 1, start - val);
+            case 2: //*
+                return (start * val <= 9, start * val);
+            case 3: ///
+                return (start % val == 0, start / val);
+            default:
+                return (false, 0);
+        }
+    }
+
+    private char GetOperationCharacter(int op)
+    {
+        switch (op)
+        {
+            case 0:
+                return '+';
+            case 1:
+                return '-';
+            case 2:
+                return 'x';
+            case 3:
+                return '/';
+            default:
+                return '?';
+        }
     }
 
     #endregion
